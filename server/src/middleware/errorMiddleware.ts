196828bec78b79af5d6a9ff7402d41eb0e5f9784
ZsomborG <zsomborg.toth@gmail.com>
2026-05-08 17:45:48 +0200

    fix(db): added error for duplicate keys

--- server/src/middleware/errorMiddleware.ts
+++ server/src/middleware/errorMiddleware.ts
@@ -2,11 +2,17 @@ import { Request, Response, NextFunction } from 'express';
 import { AppError } from '../utils/AppError';
 
 export const errorHandler = (
-  err: Error | AppError,
+  err: any,
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
+  if (err.code === 11000) {
+    const field = Object.keys(err.keyValue)[0];
+    const message = `Duplicate field value: ${field}. Please use another value!`;
+    err = new AppError(message, 400);
+  }
+
   if (err instanceof AppError) {
     return res.status(err.statusCode).json({
       message: err.message,
